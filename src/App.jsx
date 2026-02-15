18ffd1362220b595e13d7d684c0a9125d0ea8b09
Mahak Maheshwari <mahak.maheshwari06@gmail.com>
2026-02-15 08:35:48 +0530

    Fix routing issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RoleSelect from "./components/RoleSelect";
 import StudentLogin from "./components/StudentLogin";
 
-const basename =
-  import.meta.env.MODE === "production" ? "/pbl_project" : "/";
-
 function App() {
   return (
-    <Router basename={basename}>
+    <BrowserRouter basename="/pbl_project">
       <Routes>
         <Route path="/" element={<RoleSelect />} />
         <Route path="/student-login" element={<StudentLogin />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
